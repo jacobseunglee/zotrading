a7df83457cf6633060e73ac73b96b38839510d1e
Jacob Lee <jacobl11@uci.edu>
2023-02-04 14:20:22 -0800

    changeUser correctly updates user database

--- database/changeUser.js
+++ database/changeUser.js
@@ -3,8 +3,8 @@ function handleChangeUser(db)
     return function(req, res)
     {
         username = req.query.username
-        cost = req.query.cost
-        shares = req.query.shares
+        cost = parseInt(req.query.cost)
+        shares = parseInt(req.query.shares)
         stock = req.query.stock
         const users = db.collection("Users")
         updateUser(users, username, cost, shares, stock).then(x => res.send(x))
@@ -13,12 +13,16 @@ function handleChangeUser(db)
 }
 async function updateUser(users, username, cost, shares, stock)
 {
-    var update = { $inc :{}}
-    update.$inc[stock] = shares
+    var query = {};
+    query[stock] = shares;
+    console.log(query)
     const collection = await users.updateOne(
-        {id:username},
-        {$inc: {cash: cost}},
-        {update}
+    {id:username},
+    {$inc: {cash: cost}}
+    )
+    const collection1 = await users.updateOne(
+    {id:username},
+    {$inc: query}
     )
 }
 module.exports = function (db) 
