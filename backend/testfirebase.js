afae147c6cf1c9f067bb432ee0725470fa048608
Jacob Lee <jacobl11@uci.edu>
2023-02-21 01:15:05 -0800

    changed off of service account

--- backend/testfirebase.js
+++ backend/testfirebase.js
@@ -5,14 +5,14 @@ import dotenv from 'dotenv';
 
 dotenv.config()
 
-initializeApp({
-  credential: admin.credential.applicationDefault(),
-  databaseURL: 'https://zotrading-default-rtdb.firebaseio.com'
-});
-
+// initializeApp({
+//   credential: admin.credential.applicationDefault(),
+//   databaseURL: 'https://zotrading-default-rtdb.firebaseio.com'
+// });
+initializeApp(dotenv.FIREBASE_KEY)
 const db = getFirestore();
 ;
-const snapshot = await db.collection('users').get();
+const snapshot = await db.collection('stocks').get();
 snapshot.forEach((doc) => {
-  console.log(doc.id, '=>', doc.data());
+  console.log(doc.id, doc.data());
 });
